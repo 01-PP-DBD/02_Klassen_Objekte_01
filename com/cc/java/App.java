cfeb82650bf14e2d31a31ecd658170772260fed5
Danny <raducudaniel05@gmail.com>
2023-04-19 14:52:43 +0200

    update

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -9,22 +9,63 @@ public class App {
         // cat -- Referenzvariable
 
         Cat cat1 = new Cat();
-        System.out.println("Blick von aussen: " + cat1);
-        cat1.tellYourAddress();
+        output("Blick von aussen: " + cat1);
+        output(cat1.tellYourAddress());
+
+        // Wertzuweisung
+
+        // cat1.name = "Grizabella";
+        cat1.changeName("Grizabella");
+        cat1.changeFurColor("white");
+        cat1.changeAge(29);
+
+        cat1.furColor = "grey";
+
+        
+        // Ausgabe
+
+        output(cat1.tellYourName());
+        output(cat1.furColor);
+        output(Integer.toString((cat1.tellYourAge())));
+        // int---> String | 2 --> "2"
+        // output(cat1.age)
+        // output(String.valueOf(2));
+        // output(integer.toString(2));
+        // output("Ausgabe Zahl " + 2);
+
 
         System.out.println("--------------------");
 
         Cat cat2 = new Cat();
-        System.out.println("Blick von aussen: " + cat2);
-        cat2.tellYourAddress();
+        output("Blick von aussen: " + cat2);
+        output(cat2.tellYourAddress());
 
+        cat2.name = "Alonzo";
+        cat2.furColor = "grey";
+        cat2.age = 35;
 
+        output(cat2.tellYourName());
+        output(cat2.tellYourFurColor());
+        output(Integer.toString(cat2.tellYourAge()));
+
+        
+
+        
 
 
 
     }
+     public static void output(String outStr) {
 
-   
+              System.out.println(outStr);
+
+
+        
+            }
+
+    
+
+
 
 }
 
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -1,10 +1,49 @@
 package com.cc.java;
 
 public class Cat {
+
+    public String name;
+    public String furColor;
+    public int age;
+
+
+
+
+
     
     // this -- Instanzvariable
-    public void tellYourAddress() {
-        System.out.println("Blick von innen: "  + this);
+    public String tellYourAddress() {
+        return("Blick von innen: "  + this);
     }
 
+    public String tellYourName() {
+        return this.name;
+
+    }
+
+    public String tellYourFurColor() {
+        return furColor;
+        
+    }
+
+    public int tellYourAge() {
+        return this.age;
+        
+    }
+
+    public void changeName(String name) {
+         this.name = name;
+        
+    }
+    
+    public void changeFurColor(String furColor) {
+         this.furColor = furColor;
+        
+    }
+    
+    public void changeAge(int age) {
+         this.age = age;
+        
+        
+    }
 }
